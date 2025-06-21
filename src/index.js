4bedf9ff8ad0fb232e25f6ed9edadc85bc8510c4
wellesley-corpcanal <wellesley@corpcanal.com.br>
2025-06-21 14:50:48 -0300

    habilitando cors no backend

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // 1. Import the cors package
 const routes = require('./routes');
 
 const app = express();
+
+app.use(cors()); // 2. Add the cors middleware
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,4 +17,4 @@ app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
   console.log(`🚀 Server started on port ${process.env.PORT || 3333}`);
-}); 
+});
